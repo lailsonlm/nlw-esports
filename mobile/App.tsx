f9fa51bafcf1852623a2521458b2a892f81527f4
Lailson Sobral <lailsonlm@hotmail.com>
2022-09-15 17:47:43 -0300

    create backend server, develop web and mobile frontend

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [isFontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black })
+
+  return (
+    <Background>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { isFontsLoaded ? <Home /> : <Loading /> }
+    </Background>
+  );
+}
---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import { ImageBackground } from 'react-native';
+
+import backgroundImg  from '../../assets/background-galaxy.png';
+import { styles } from './styles';
+
+interface BackgroundProps {
+  children: ReactNode;
+}
+
+export function Background({ children }: BackgroundProps) {
+  return (
+    <ImageBackground 
+      source={backgroundImg}
+      defaultSource={backgroundImg}
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
